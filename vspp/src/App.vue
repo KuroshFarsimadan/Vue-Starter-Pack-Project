0cd0ddb12304378917a33b64da335ddc92c5cdb8
Kurosh Farsimadan <kurosh_farsimadan@yahoo.com>
2018-11-05 15:17:04 -0500

    Add files via upload

---
+++ vspp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header/>
+    <div class="app-content">
+      <Home/>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./components/Home.vue";
+
+export default {
+  name: "app",
+  components: {
+    Footer,
+    Header,
+    Home
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vspp/src/main.js
@@ -0,0 +1,44 @@
+/* import Vue from 'vue';
+import App from './App.vue';
+import 'vue-tel-input/dist/vue-tel-input.css';
+import VueTelInput from 'vue-tel-input';
+
+Vue.use(VueTelInput);
+Vue.config.productionTip = false;
+
+export const bus = new Vue();
+
+new Vue({
+
+  render: h => h(App),
+}).$mount('#app') */
+
+import App from './App.vue';
+import State1 from "./components/State1.vue";
+import State2 from "./components/State2.vue";
+import State3 from "./components/State3.vue";
+import State4 from "./components/State4.vue";
+import 'vue-tel-input/dist/vue-tel-input.css';
+import VueTelInput from 'vue-tel-input';
+
+export const bus = new Vue();
+
+Vue.use(VueTelInput);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/', component: App, redirect: 'State1',
+      children: [
+        { path: 'State1', component: State1 },
+        { path: 'State2', component: State2 },
+        { path: 'State3', component: State3 },
+        { path: 'State4', component: State4 },
+      ]
+    }
+  ]
+});
+
+new Vue({
+  router
+}).$mount('#app');
